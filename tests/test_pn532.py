a28dbdaec85871ae24a66ffcb3f517d59159abda
bibi21000 <bibi21000@gmail.com>
2016-04-24 11:31:03 +0200

    Fix

--- tests/test_pn532.py
+++ tests/test_pn532.py
@@ -44,7 +44,7 @@ from janitoo.utils import TOPIC_NODES, TOPIC_NODES_REPLY, TOPIC_NODES_REQUEST
 from janitoo.utils import TOPIC_BROADCAST_REPLY, TOPIC_BROADCAST_REQUEST
 from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_SYSTEM, TOPIC_VALUES_BASIC
 
-import janitoo_raspberry_spi_pn532.PN532Component
+from janitoo_raspberry_spi_pn532.pn532 import PN532Component
 
 class TestPN532Component(JNTTComponent, JNTTComponentCommon):
     """Test the component
